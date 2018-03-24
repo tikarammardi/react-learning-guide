65cd151e536945e075defa1e84b86dd4b28a428a
Tikaram Mardi <tikarammardi@gmail.com>
2018-03-24 16:57:35 +0530

    added class name dynamically

--- src/App.js
+++ src/App.js
@@ -49,7 +49,8 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -72,12 +73,25 @@ class App extends Component {
         })}
         </div>
       );
+      style.backgroundColor = 'red';
+    }
+
+    //let classes = ['red', 'bold'].join(' '); // "red bold"
+     
+    const classes = [];
+
+    if(this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }
+
+    if(this.state.persons.length <=1) {
+      classes.push('bold'); // classes = ['red', 'bold']
     }
 
     return (
       <div className="App">
             <h1>Hello I am React app</h1>
-            <p>This is really working </p>
+            <p className={classes.join(' ')}>This is really working </p>
             <button 
             style={style}
             onClick={() => this.togglePersonsHandler("Mewow")}>Toggle Person</button>
